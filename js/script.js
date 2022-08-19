f78dec2be97e9d98ec8508cf9bbb1d0630d9ebb5
NguyenTaiAnh <anh.nguyen@tpisoftware.com>
2022-08-19 17:05:59 +0700

    add animation, login, register

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
-var ratio = window.devicePixelRatio || 1;
-var w = screen.width * ratio;
-var h = screen.height * ratio;
-console.log("check log: ", w < 768)
-console.log("check log: ", w )
-if (w < 768) {
-    alert('Less than 960');
+// var ratio = window.devicePixelRatio || 1;
+// var w = screen.width * ratio;
+// var h = screen.height * ratio;
+// console.log("check log: ", w < 768)
+// console.log("check log: ", w )
+// if (w < 768) {
+//     alert('Less than 960');
+// }
+// else {
+// // alert('More than 960');
+// }
+
+
+const words = ["suppose end get","boy warrant general","natural. delightful","met sufficient projection ask.","decisively everything","principles if preference","do","impression","of. preserved oh so","difficult repulsive on","in household. in what","do","miss time be. valley","as be","appear","cannot so","by.","convinced resembled dependent","remainder led zealously","his shy own","belonging. always length","letter","adieus","add number moment she.","promise few","compass six several old","offices removal parties","fat. concluded","rapturous it intention","perfectly daughters","is as.","behaviour we","improving at something","to. evil true","high lady roof men","had open.","to projection considered it","precaution an","melancholy or.","wound young","you thing","worse along being ham.","dissimilar of favourable solicitude","if sympathize middletons","at. forfeited","up if disposing","perfectly in an","eagerness perceived necessary.","belonging sir","curiosity discovery","extremity yet","forfeited prevailed","own off.","travelling by","introduced of","mr terminated. knew as","miss","my high hope quit. in","curiosity shameless dependent","knowledge up.","literature admiration","frequently indulgence announcing","are who you","her. was","least quick after","six. so","it yourself repeated","together","cheerful. neither it cordial so","painful picture studied if.","sex","him position doubtful","resolved boy expenses.","her engrossed deficient","northward and neglected favourite newspaper.","but","use peculiar","produced concerns ten. maids","table how","learn","drift","but purse","stand yet","set. music me","house could","among oh","as their. piqued","our","sister shy nature almost","his","wicket.","hand dear","so we hour","to. he","we be","hastily","offence effects","he service. sympathize","it projection","ye insipidity celebrated"]
+setTimeout(() =>{
+    const containerEl = document.querySelector('.container')
+    const formEl = document.querySelector('#search')
+    // const formEl = document.getElementById("search")
+// const formEl = document.getElementById("search")
+    const dropEl = document.querySelector('.drop')
+const  formHandler = async (e) => {
+    const userInput = e.target.value.toLowerCase()
+
+    if(userInput.length === 0) {
+        dropEl.style.height = 0
+        return dropEl.innerHTML = ''              
+    }
+
+    const filteredWords = words.filter(word => word.toLowerCase().includes(userInput)).sort().splice(0, 5)
+    
+    dropEl.innerHTML = ''
+    filteredWords.forEach(item => {
+        const listEl = document.createElement('li')
+        const aEl = document.createElement('a')
+        aEl.setAttribute('href', "#")
+        listEl.appendChild(aEl)
+        aEl.textContent = item
+        aEl.classList.add('match')
+        if(item === userInput) {
+            aEl.classList.add('match')
+        }
+        dropEl.appendChild(listEl)
+    })
+
+    if(dropEl.children[0] === undefined) {
+        return dropEl.style.height = 0
+    }
+
+    let totalChildrenHeight = dropEl.children[0].offsetHeight * filteredWords.length
+    dropEl.style.height = totalChildrenHeight + 'px'
+
 }
-else {
-// alert('More than 960');
-}
+
+formEl.addEventListener('input', formHandler)
+},100)
